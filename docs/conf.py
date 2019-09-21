e9da143eb815dc456bf7e2516b0526715df6c8f0
Leah Wasser <leah.wasser@colorado.edu>
2019-09-21 13:10:01 -0600

    black

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,14 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "abcclassroom/tests",
+    "keepme",
+]
+
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
@@ -146,7 +153,9 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "abc-classroom", "abc-classroom Documentation", [author], 1)]
+man_pages = [
+    (master_doc, "abc-classroom", "abc-classroom Documentation", [author], 1)
+]
 
 
 # -- Options for Texinfo output ----------------------------------------------
