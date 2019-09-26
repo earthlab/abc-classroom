2ad952e7a722b65b77dd7c9f16ec1f75933abb9c
Leah Wasser <leah.wasser@colorado.edu>
2019-09-26 17:20:55 -0600

    Bump version: 0.0.6 → 0.0.7

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2019, Earth Lab"
 author = "Leah Wasser, Tim Head"
 
 # The short X.Y version
-version = "0.0.6"
+version = "0.0.7"
 # The full version, including alpha/beta/rc tags
-release = "0.0.6"
+release = "0.0.7"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name="abc-classroom",
-    version="0.0.6",
+    version="0.0.7",
     description="Efficiently manage github classroom assignments from the command line.",
     long_description="Authoring and grading of notebook assignments",
     license="BSD",
