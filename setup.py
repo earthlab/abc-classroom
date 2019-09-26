6d8b2ff62ddd8e18884a09d47ad35e4b81d2279a
Leah Wasser <leah.wasser@colorado.edu>
2019-09-26 17:31:26 -0600

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name="abc-classroom",
-    version="0.0.7",
+    version="0.0.8",
     description="Efficiently manage github classroom assignments from the command line.",
     long_description="Authoring and grading of notebook assignments",
     license="BSD",
