8b3cd91798807e287ed69beacdf12e324ca6d7a9
Nathan Korinek <nako1890@colorado.edu>
2019-10-08 14:57:55 -0600

    Fixed a few docstrings

--- abcclassroom/quickstart.py
+++ abcclassroom/quickstart.py
@@ -12,12 +12,20 @@ import abcclassroom
 
 def path_to_example(dataset):
     """
-    Gets the path to an example dataset.
+    Construct a file path to an example dataset.
+    This file defines helper functions to access data files in this directory,
+    to support examples. Adapted from the PySAL package.
 
     Parameters
     ----------
-    dataset : string
-        stringy boi
+    dataset: string
+        Name of a dataset to access (e.g., "sample_config.yml")
+            
+    Returns
+    -------
+    string
+        A file path (string) to the dataset
+
     """
     abcclassroom_path = os.path.split(abcclassroom.__file__)[0]
     data_dir = os.path.join(abcclassroom_path, "example-data")
@@ -29,18 +37,8 @@ def path_to_example(dataset):
 
 def create_dir_struct():
     """
-    Construct a file path to an example dataset.
-    This file defines helper functions to access data files in this directory,
-    to support examples. Adapted from the PySAL package.
-
-    Parameters
-    ----------
-    dataset: string
-        Name of a dataset to access (e.g., "epsg.json", or "RGB.byte.tif")
-
-    Returns
-    -------
-        A file path (string) to the dataset
+    Creates a directory structure that can be used to start an abc-classroom course. This includes a main directory,
+    two sub directories for templates and cloned files, and a sample configuration file.
     """
     # Creating the default names for the cloned and template repos.
     course = "course_dir"
@@ -51,7 +49,7 @@ def create_dir_struct():
             "Configuration file can't be located, please ensure abc-classroom has been installed correctly"
         )
     # Allows users to rename the cloned and template repos.
-    parser = argparse.ArgumentParser(description=create_dir_struct().__doc__)
+    parser = argparse.ArgumentParser(description=create_dir_struct.__doc__)
     parser.add_argument(
         "--course_name",
         help="Name of the main course repository"
@@ -80,6 +78,12 @@ def create_dir_struct():
     os.mkdir(cloned_dir)
     os.mkdir(template_dir)
     shutil.copy(config, main_dir)
+    if args.course_name:
+        with open(os.path.join(course, "sample_config.yml"), "r") as file:
+            filedata = file.read()
+            filedata = filedata.replace("earth-analytics-bootcamp", args.course_name)
+        with open(os.path.join(course, "sample_config.yml"), "w") as file:
+            file.write(filedata)
     print("""
     Directory structure created to begin using abc-classroom. All directories needed and a sample configuration file 
     have been created. To proceed, please move your sample roster and nbgrader directory into the main directory 
