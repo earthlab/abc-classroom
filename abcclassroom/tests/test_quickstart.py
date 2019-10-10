a0e11b6bdbddcc2d5f64b81cf77484c1a2f33d87
Nathan Korinek <nako1890@colorado.edu>
2019-10-10 15:50:32 -0600

    Added tests to make sure abc-quickstart works

--- abcclassroom/tests/test_quickstart.py
+++ abcclassroom/tests/test_quickstart.py
@@ -1,57 +1,80 @@
-from pytest import raises
 import os
 import subprocess
 from shutil import rmtree
 
 
 def test_quickstart_default():
-    os.system("abc-quickstart")
+    """
+    Test that the standard run of abc-quickstart creates all expected folders and outputs.
+    """
+    output = subprocess.run("abc-quickstart", capture_output=True)
     main_dir = os.path.join(os.getcwd(), "course-dir")
-    assert (
-        os.path.isdir(main_dir)
-        and os.path.isdir(os.path.join(main_dir, "assignment_repos"))
-        and os.path.isdir(os.path.join(main_dir, "cloned_dirs"))
-        and os.path.isfile(os.path.join(main_dir, "config.yml"))
-    )
+    with open(os.path.join(main_dir, "config.yml")) as data:
+        assert (
+            os.path.isdir(main_dir)
+            and os.path.isdir(os.path.join(main_dir, "assignment_repos"))
+            and os.path.isdir(os.path.join(main_dir, "cloned_dirs"))
+            and os.path.isfile(os.path.join(main_dir, "config.yml"))
+            and "Directory structure created" in str(output.stdout)
+            and "course-name" in data.read()
+        )
     rmtree(main_dir)
 
 
 def test_quickstart_custom_name():
-    os.system("abc-quickstart --course_name python_test_dir_custom_name")
+    """
+    Test that abc-quickstart works with a custom name.
+    """
+    output = subprocess.run("abc-quickstart --course_name python_test_dir_custom_name", capture_output=True)
     main_dir = os.path.join(os.getcwd(), "python_test_dir_custom_name")
-    assert (
-        os.path.isdir(main_dir)
-        and os.path.isdir(os.path.join(main_dir, "assignment_repos"))
-        and os.path.isdir(os.path.join(main_dir, "cloned_dirs"))
-        and os.path.isfile(os.path.join(main_dir, "config.yml"))
-    )
+    with open(os.path.join(main_dir, "config.yml")) as data:
+        assert (
+            os.path.isdir(main_dir)
+            and os.path.isdir(os.path.join(main_dir, "assignment_repos"))
+            and os.path.isdir(os.path.join(main_dir, "cloned_dirs"))
+            and os.path.isfile(os.path.join(main_dir, "config.yml"))
+            and "Directory structure created" in str(output.stdout)
+            and "python_test_dir_custom_name" in data.read()
+        )
     rmtree(main_dir)
 
 
 def test_quickstart_bad_name():
-    stderr = subprocess.run(
-        "abc-quickstart --course_name 'bad name'", capture_output=True
+    """
+    Test that abc-quickstart fails with a improperly formatted name.
+    """
+    output = subprocess.run(
+        'abc-quickstart --course_name "bad name"', capture_output=True
     )
-    assert "Spaces " in stderr
+    assert "Spaces" in str(output.stderr)
 
 
 def test_quickstart_remake_existing():
-    os.system("abc-quickstart --course_name python_test_dir_custom_name")
-    stderr = stderr = subprocess.run(
+    """
+    Test that abc-quickstart fails when using the same name for a course twice.
+    """
+    subprocess.run("abc-quickstart --course_name python_test_dir_custom_name")
+    output = subprocess.run(
         "abc-quickstart --course_name abc-quickstart --course_name python_test_dir_custom_name",
         capture_output=True,
     )
-    assert "Ooops! " in stderr
+    assert "Ooops! " in str(output.stderr)
 
 
 def test_quickstart_remove_existing():
-    os.system("abc-quickstart --course_name python_test_dir_custom_name")
-    os.system("abc-quickstart --course_name python_test_dir_custom_name -f")
+    """
+    Test that abc-quickstart doesn't fail when using the same name for a course twice and the -f argument.
+    """
+    subprocess.run("abc-quickstart --course_name python_test_dir_custom_name")
+    output = subprocess.run("abc-quickstart --course_name python_test_dir_custom_name -f", capture_output=True)
     main_dir = os.path.join(os.getcwd(), "python_test_dir_custom_name")
-    assert (
-        os.path.isdir(main_dir)
-        and os.path.isdir(os.path.join(main_dir, "assignment_repos"))
-        and os.path.isdir(os.path.join(main_dir, "cloned_dirs"))
-        and os.path.isfile(os.path.join(main_dir, "config.yml"))
-    )
+    with open(os.path.join(main_dir, "config.yml")) as data:
+        assert (
+            os.path.isdir(main_dir)
+            and os.path.isdir(os.path.join(main_dir, "assignment_repos"))
+            and os.path.isdir(os.path.join(main_dir, "cloned_dirs"))
+            and os.path.isfile(os.path.join(main_dir, "config.yml"))
+            and "Directory structure created" in str(output.stdout)
+            and "python_test_dir_custom_name" in data.read()
+        )
     rmtree(main_dir)
