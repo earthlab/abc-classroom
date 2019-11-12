e6785a1cc62b96185c546b379c05be74b618d837
Leah Wasser <leah.wasser@colorado.edu>
2019-11-12 16:05:02 -0700

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    version="0.0.11",
+    version="0.0.12",
     license="BSD",
     packages=["abcclassroom"],
     install_requires=[
