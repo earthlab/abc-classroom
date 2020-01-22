8deaa1b46250a01e87cefdeac409c3fc12ff01e8
Karen Cranston <karen.cranston@gmail.com>
2020-01-22 10:18:37 -0500

    update rest of quickstart tests to use tmp_path

--- abcclassroom/tests/test_quickstart.py
+++ abcclassroom/tests/test_quickstart.py
@@ -1,6 +1,7 @@
 import os
 import pytest
-from shutil import rmtree
+
+# from shutil import rmtree
 from abcclassroom.quickstart import create_dir_struct as quickstart
 
 
@@ -11,7 +12,6 @@ def test_quickstart_default(tmp_path):
     """
     quickstart(working_dir=tmp_path)
     main_dir = os.path.join(tmp_path, "course_dir")
-    # main_dir = os.path.join(os.getcwd(), "course_dir")
     assert os.path.isdir(main_dir)
     assert os.path.isfile(os.path.join(main_dir, "config.yml"))
     with open(os.path.join(main_dir, "config.yml")) as data:
@@ -22,27 +22,22 @@ def test_quickstart_default(tmp_path):
         )
     assert os.path.isdir(os.path.join(main_dir, "template_dir"))
     assert os.path.isdir(os.path.join(main_dir, "clone_dir"))
-    rmtree(main_dir)
+    # rmtree(main_dir)
 
 
-def test_quickstart_custom_name():
+def test_quickstart_custom_name(tmp_path):
     """
     Test that abc-quickstart works with a custom name.
     """
     custom_name = "pytest_dir_custom_name"
-    quickstart(custom_name)
-    main_dir = os.path.join(os.getcwd(), custom_name)
+    quickstart(custom_name, working_dir=tmp_path)
+    main_dir = os.path.join(tmp_path, custom_name)
+    assert os.path.isdir(main_dir)
+    assert os.path.isfile(os.path.join(main_dir, "config.yml"))
     with open(os.path.join(main_dir, "config.yml")) as data:
-        assert (
-            os.path.isdir(main_dir)
-            and os.path.isdir(os.path.join(main_dir, "template_dir"))
-            and os.path.isdir(os.path.join(main_dir, "clone_dir"))
-            and os.path.isfile(os.path.join(main_dir, "config.yml"))
-            and "python_test_dir_custom_name"
-            and "template_dir"
-            and "clone_dir" in data.read()
-        )
-    rmtree(main_dir)
+        assert custom_name and "template_dir" and "clone_dir" in data.read()
+    assert os.path.isdir(os.path.join(main_dir, "template_dir"))
+    assert os.path.isdir(os.path.join(main_dir, "clone_dir"))
 
 
 def test_quickstart_bad_name():
@@ -53,32 +48,31 @@ def test_quickstart_bad_name():
         quickstart("bad name")
 
 
-def test_quickstart_remake_existing():
+def test_quickstart_remake_existing(tmp_path):
     """
     Test that abc-quickstart fails when using the same name for a course twice.
     """
-    quickstart("python_test_dir_custom_name")
-    main_dir = os.path.join(os.getcwd(), "python_test_dir_custom_name")
+    quickstart("python_test_dir_custom_name", working_dir=tmp_path)
+    main_dir = os.path.join(tmp_path, "python_test_dir_custom_name")
     with pytest.raises(ValueError, match="Ooops! "):
-        quickstart("python_test_dir_custom_name")
-    rmtree(main_dir)
+        quickstart("python_test_dir_custom_name", working_dir=tmp_path)
 
 
-def test_quickstart_remove_existing():
+def test_quickstart_remove_existing(tmp_path):
     """
     Test that abc-quickstart doesn't fail when using the same name for a course twice and the -f argument.
     """
-    quickstart("python_test_dir_custom_name")
-    quickstart("python_test_dir_custom_name", True)
-    main_dir = os.path.join(os.getcwd(), "python_test_dir_custom_name")
+    custom_name = "python_test_dir_custom_name"
+    quickstart(custom_name, working_dir=tmp_path)
+    quickstart(custom_name, True, working_dir=tmp_path)
+    main_dir = os.path.join(tmp_path, custom_name)
+    assert os.path.isdir(main_dir)
+    assert os.path.isfile(os.path.join(main_dir, "config.yml"))
     with open(os.path.join(main_dir, "config.yml")) as data:
         assert (
-            os.path.isdir(main_dir)
-            and os.path.isdir(os.path.join(main_dir, "template_dir"))
-            and os.path.isdir(os.path.join(main_dir, "clone_dir"))
-            and os.path.isfile(os.path.join(main_dir, "config.yml"))
-            and "course-name"
+            "course_directory"
             and "template_dir"
             and "clone_dir" in data.read()
         )
-    rmtree(main_dir)
+    assert os.path.isdir(os.path.join(main_dir, "template_dir"))
+    assert os.path.isdir(os.path.join(main_dir, "clone_dir"))
