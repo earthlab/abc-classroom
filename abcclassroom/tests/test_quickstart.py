77548060ca29613585013e746c7cfeb490b28f36
Karen Cranston <karen.cranston@gmail.com>
2020-02-11 13:30:26 -0500

    add test for path_to_example

--- abcclassroom/tests/test_quickstart.py
+++ abcclassroom/tests/test_quickstart.py
@@ -1,6 +1,17 @@
 import os
 import pytest
-from abcclassroom.quickstart import create_dir_struct as quickstart
+import abcclassroom.quickstart as quickstart
+
+
+def test_path_to_example():
+    # with filename that should exist
+    filename = "config.yml"
+    config_path = quickstart.path_to_example(filename)
+
+    # with filename that doesn't exist
+    filename = "filethatdoesntexist.txt"
+    with pytest.raises(FileNotFoundError):
+        config_path = quickstart.path_to_example(filename)
 
 
 def test_quickstart_default(tmp_path):
@@ -8,7 +19,7 @@ def test_quickstart_default(tmp_path):
     Test that abc-quickstart without arguments creates the default
     "course_dir" main directory and all expected folders and outputs.
     """
-    quickstart(working_dir=tmp_path)
+    quickstart.create_dir_struct(working_dir=tmp_path)
     # check that main dir and config created
     main_dir = os.path.join(tmp_path, "course_dir")
     assert os.path.isdir(main_dir)
@@ -30,7 +41,7 @@ def test_quickstart_custom_name(tmp_path):
     Test that abc-quickstart works with a custom name.
     """
     custom_name = "pytest_dir_custom_name"
-    quickstart(custom_name, working_dir=tmp_path)
+    quickstart.create_dir_struct(custom_name, working_dir=tmp_path)
     main_dir = os.path.join(tmp_path, custom_name)
     assert os.path.isdir(main_dir)
     assert os.path.isfile(os.path.join(main_dir, "config.yml"))
@@ -45,16 +56,20 @@ def test_quickstart_bad_name():
     Test that abc-quickstart fails with a improperly formatted name.
     """
     with pytest.raises(ValueError, match="Spaces not"):
-        quickstart("bad name")
+        quickstart.create_dir_struct("bad name")
 
 
 def test_quickstart_remake_existing(tmp_path):
     """
     Test that abc-quickstart fails when using the same name for a course twice.
     """
-    quickstart("python_test_dir_custom_name", working_dir=tmp_path)
+    quickstart.create_dir_struct(
+        "python_test_dir_custom_name", working_dir=tmp_path
+    )
     with pytest.raises(ValueError, match="Ooops! "):
-        quickstart("python_test_dir_custom_name", working_dir=tmp_path)
+        quickstart.create_dir_struct(
+            "python_test_dir_custom_name", working_dir=tmp_path
+        )
 
 
 def test_quickstart_remove_existing(tmp_path):
@@ -62,8 +77,8 @@ def test_quickstart_remove_existing(tmp_path):
     Test that abc-quickstart doesn't fail when using the same name for a course twice and the -f argument.
     """
     custom_name = "python_test_dir_custom_name"
-    quickstart(custom_name, working_dir=tmp_path)
-    quickstart(custom_name, True, working_dir=tmp_path)
+    quickstart.create_dir_struct(custom_name, working_dir=tmp_path)
+    quickstart.create_dir_struct(custom_name, True, working_dir=tmp_path)
     main_dir = os.path.join(tmp_path, custom_name)
     assert os.path.isdir(main_dir)
     assert os.path.isfile(os.path.join(main_dir, "config.yml"))
