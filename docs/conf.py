815d94fdb276d29ae9f34943f42aa85d51250508
lwasser <leah.wasser@colorado.edu>
2020-09-16 17:31:45 -0600

    Bump version: 0.1.2 → 0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2020, Earth Lab"
 author = "Leah Wasser, Karen Cranston"
 
 # The short X.Y version
-version = "0.1.2"
+version = "0.1.3"
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    version="0.1.2",
+    version="0.1.3",
     license="BSD",
     packages=["abcclassroom"],
     install_requires=[
