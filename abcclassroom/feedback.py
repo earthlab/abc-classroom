299da116562bf7fe54a590f6eaf6590cde1bfd35
lwasser <leah.wasser@colorado.edu>
2020-09-26 10:48:45 -0600

    black

--- abcclassroom/feedback.py
+++ abcclassroom/feedback.py
@@ -20,7 +20,7 @@ def copy_feedback(args):
     course_materials/feedback/student/assignment following a typical
     nbgrader structure. Copies all files in the source directory.
     """
-    assignment = args.assignment
+    assignment_name = args.assignment
     do_github = args.github
 
     print("Loading configuration from config.yml")
@@ -38,12 +38,12 @@ def copy_feedback(args):
             reader = csv.DictReader(csvfile)
             for row in reader:
                 student = row["github_username"]
-                source_files = Path(feedback_dir, student, assignment).glob(
-                    "*"
-                )
-                repo = "{}-{}".format(assignment, student)
-                # The repos now live in clone_dir/assignment-name/repo
-                destination_dir = Path(clone_dir, assignment, repo)
+                source_files = Path(
+                    feedback_dir, student, assignment_name
+                ).glob("*")
+                repo_name = "{}-{}".format(assignment_name, student)
+                # The repos now live in clone_dir/assignment-name/repo-name
+                destination_dir = Path(clone_dir, assignment_name, repo_name)
                 if not destination_dir.is_dir():
                     print(
                         "Local student repository {} does not exist; skipping "
@@ -59,7 +59,9 @@ def copy_feedback(args):
                     shutil.copy(f, destination_dir)
                 github.commit_all_changes(
                     destination_dir,
-                    msg="Adding feedback for assignment {}".format(assignment),
+                    msg="Adding feedback for assignment {}".format(
+                        assignment_name
+                    ),
                 )
                 if do_github:
                     github.push_to_github(destination_dir)
