ff742936998615ae6d8569689516e601bbcfd31a
Leah Wasser <leah.wasser@colorado.edu>
2020-09-29 14:20:29 -0600

    black

---
+++ abcclassroom/tests/test_feedback_report.py
@@ -0,0 +1,16 @@
+# This will require having a dummy notebook fixture which represents a
+# student assignment
+
+# Test that a notebook with ### BEGIN HIDDEN TESTS gets scrubbed properly
+
+# Test what happens when they misspell the assignment name - it should fail
+# gracefully
+
+# Test what happens when the autograded directory for a student doesn't exist
+
+# Test what happens when the feedback directory for a student doesn't exist
+
+# Test what happens when the autograded and feedback directories (the root dir
+# don't exist
+
+# Test what happens when there are no notebooks to open
