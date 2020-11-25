11f75abe7fd563bfca54834d55a1f8dacfa053e5
Leah Wasser <leah.wasser@colorado.edu>
2020-11-24 19:21:19 -0700

    lint

--- abcclassroom/tests/conftest.py
+++ abcclassroom/tests/conftest.py
@@ -13,6 +13,24 @@ import abcclassroom.config as cf
 
 
 @pytest.fixture
+def test_data():
+    """Creates a  course structure with student and file names to ensure
+    consistent creation of elements in all fixtures"""
+    test_data = {
+        "assignment": "assignment1",
+        "students": ["bert", "alana"],
+        "files": [
+            "nb1.ipynb",
+            "nb2.ipynb",
+            "script.py",
+            "junk.csv",
+            ".DS_Store",
+        ],
+    }
+    return test_data
+
+
+@pytest.fixture
 def default_config():
     """
     A config dictionary with default values.
@@ -42,29 +60,51 @@ def sample_course_structure(tmp_path):
     # Get config and reset course path - for some reason if the path isn't
     # set here it grabs the config in abc-classroom (tim's old config)
     path_to_course = Path(tmp_path, course_name)
-    a_config = cf.get_config(configpath=path_to_course)
-    a_config["course_directory"] = path_to_course
+    config = cf.get_config(configpath=path_to_course)
+    config["course_directory"] = path_to_course
     os.chdir(path_to_course)
-    return course_name, a_config
+    return course_name, config
 
 
 @pytest.fixture
-def course_structure_assignment(sample_course_structure, tmp_path):
+def course_structure_assignment(sample_course_structure, tmp_path, test_data):
     """Creates an assignment within the default course structure directory
     with several files including system files that we want to ignore."""
-    course_name, a_config = sample_course_structure
-    assignment_name = "demo_assignment1"
+    course_name, config = sample_course_structure
+    assignment_name = test_data["assignment"]
     release_path = Path(
-        tmp_path,
-        course_name,
-        a_config["course_materials"],
+        config["course_directory"],
+        config["course_materials"],
         "release",
         assignment_name,
     )
     release_path.mkdir(parents=True)
-    release_path.joinpath("file1.txt").touch()
-    release_path.joinpath("file2.ipynb").touch()
-    release_path.joinpath(".DS_Store").touch()
-    print(release_path)
+    # Create all assignment files listed in the test_data fixture
+    for a_file in test_data["files"]:
+        release_path.joinpath(a_file).touch()
+    return config, assignment_name, release_path
 
-    return a_config, assignment_name, release_path
+
+@pytest.fixture
+def course_with_student_clones(
+    course_structure_assignment, tmp_path, test_data
+):
+    """
+    Creates the final piece of a typical course including student cloned
+    repos for the assignment. Student names and list of files come
+    from test_data fixture
+    """
+    config, assignment_name, release_path = course_structure_assignment
+
+    clone_dir = config["clone_dir"]
+    students = test_data["students"]
+    # Loop through and create a clone repo for each student and add files
+    for s in students:
+        repo_name = "{}-{}".format(assignment_name, s)
+        assignment_path = Path(
+            config["course_directory"], clone_dir, assignment_name, repo_name
+        )
+        assignment_path.mkdir(parents=True, exist_ok=True)
+        for f in test_data["files"]:
+            Path(assignment_path, f).touch()
+    return config, assignment_name, students
--- abcclassroom/tests/test_clone.py
+++ abcclassroom/tests/test_clone.py
@@ -1,22 +1,19 @@
 # Tests for clone script
+from pathlib import Path
 
 import pytest
-from pathlib import Path
 
 import abcclassroom.clone as abcclone
 
-# TODO - this should be a fixture
-test_data = {
-    "assignment": "assignment1",
-    "students": ["bert", "alana"],
-    "files": ["nb1.ipynb", "nb2.ipynb", "script.py", "junk.csv"],
-}
 
 # TODO: We might have a version of this fixture in conftest
 
+# TODO remove this fixture potentially given  what is in conftest -
+#  course_with_student_clones
+
 
 @pytest.fixture
-def test_files(default_config, tmp_path):
+def test_files(default_config, tmp_path, test_data):
     """
     Creates the directories for cloning student repos and for holding
     submitted material. Creates two notebook and one non-notebook files.
@@ -105,58 +102,72 @@ def test_roster_wrong_format(sample_course_structure):
 #     # when skip_existing is no
 # TODO test that when files_to_ignore is not populated it fails gracefully
 
-# TODO test that when giles_to_grade  is not populated it fails gracefully..
+# TODO test that when files_to_grade  is not populated it fails gracefully..
 
 
-def test_copy_assignment_files(default_config, test_files):
+def test_copy_assignment_files(course_with_student_clones):
+    """Test that copy assignment files moves files as expected"""
+
+    config, assignment_name, students = course_with_student_clones
+
     materials_dir = Path(
-        default_config["course_directory"], default_config["course_materials"]
+        config["course_directory"], config["course_materials"]
     )
-    assignment = test_data["assignment"]
-    students = test_data["students"]
+
     for s in students:
-        abcclone.copy_assignment_files(default_config, s, assignment)
+        abcclone.copy_assignment_files(config, s, assignment_name)
 
         good_files = ["nb1.ipynb", "nb2.ipynb", "script.py"]
-
+        submitted_path = Path(materials_dir, "submitted", s, assignment_name)
         for file in good_files:
-            assert Path(
-                materials_dir, "submitted", s, assignment, file
-            ).exists()
-        assert (
-            Path(
-                materials_dir, "submitted", s, assignment, "junk.csv"
-            ).exists()
-            is False
-        )
+            assert Path(submitted_path, file).exists()
+        assert Path(submitted_path, "junk.csv").exists() is False
 
 
-def test_copy_assignment_files_optional_files(
-    default_config, test_files, tmp_path
-):
-    materials_dir = Path(
-        default_config["course_directory"], default_config["course_materials"]
-    )
-    assignment = test_data["assignment"]
-    students = test_data["students"]
-    default_config["files_to_ignore"] = [
-        ".DS_Store",
-        ".ipynb_checkpoints",
-        "junk.csv",
-        "*.py",
-    ]
-    for s in students:
-        abcclone.copy_assignment_files(default_config, s, assignment)
+#
+def test_files_to_grade_empty(course_with_student_clones, tmp_path):
+    """Test that when files to grade list is empty, only ipynb files are
+    moved"""
 
-        good_files = ["nb1.ipynb", "nb2.ipynb"]
+    config, assignment_name, students = course_with_student_clones
 
-        for file in good_files:
-            assert Path(
-                materials_dir, "submitted", s, assignment, file
-            ).exists()
-        assert (
-            Path(
-                materials_dir, "submitted", s, assignment, "junk.csv"
-            ).exists()
-            is False
-        )
+    # materials_dir = Path(
+    #    default_config["course_directory"],default_config["course_materials"]
+    # )
+
+    # Clear out the files_to_grade  item
+    config["files_to_grade"] = []
+    for a_student in students:
+        abcclone.copy_assignment_files(config, a_student, assignment_name)
+
+
+# def test_copy_assignment_files_optional_files(
+#     default_config, test_files, tmp_path
+# ):
+#     """Test that files to skip are not moved by copy assignment files"""
+#     materials_dir = Path(
+#         default_config["course_directory"],default_config["course_materials"]
+#     )
+#     assignment = test_data["assignment"]
+#     students = test_data["students"]
+#     default_config["files_to_ignore"] = [
+#         ".DS_Store",
+#         ".ipynb_checkpoints",
+#         "junk.csv",
+#         "*.py",
+#     ]
+#     for s in students:
+#         abcclone.copy_assignment_files(default_config, s, assignment)
+#
+#         good_files = ["nb1.ipynb", "nb2.ipynb"]
+#
+#         for file in good_files:
+#             assert Path(
+#                 materials_dir, "submitted", s, assignment, file
+#             ).exists()
+#         assert (
+#             Path(
+#                 materials_dir, "submitted", s, assignment, "junk.csv"
+#             ).exists()
+#             is False
+#         )
