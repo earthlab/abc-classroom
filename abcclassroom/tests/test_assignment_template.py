ecead8b23dd96d47323a62c98d32dd5916d0ecf8
Leah Wasser <leah.wasser@colorado.edu>
2020-11-24 19:49:31 -0700

    remove duplicate fixtures

--- abcclassroom/tests/test_assignment_template.py
+++ abcclassroom/tests/test_assignment_template.py
@@ -5,7 +5,6 @@ import os
 from pathlib import Path
 import shutil
 
-from abcclassroom.quickstart import create_dir_struct
 import abcclassroom.template as abctemplate
 import abcclassroom.github as github
 import abcclassroom.config as cf
@@ -19,43 +18,6 @@ import abcclassroom.config as cf
 def config_file(default_config, tmp_path):
     """Writes the config to a file in tmp_path"""
     cf.write_config(default_config, tmp_path)
-
-
-@pytest.fixture
-def sample_course_structure(tmp_path):
-    """Creates the quickstart demo directory setup for testing"""
-    course_name = "demo-course"
-    # Run quickstart
-    create_dir_struct(course_name, working_dir=tmp_path)
-    # Get config and reset course path - for some reason if the path isn't
-    # set here it grabs the config in abc-classroom (tim's old config)
-    path_to_course = Path(tmp_path, course_name)
-    a_config = cf.get_config(configpath=path_to_course)
-    a_config["course_directory"] = path_to_course
-    os.chdir(path_to_course)
-    return course_name, a_config
-
-
-@pytest.fixture
-def course_structure_assignment(sample_course_structure, tmp_path):
-    """Creates an assignment within the default course structure directory
-    with several files including system files that we want to ignore."""
-    course_name, a_config = sample_course_structure
-    assignment_name = "demo_assignment1"
-    release_path = Path(
-        tmp_path,
-        course_name,
-        a_config["course_materials"],
-        "release",
-        assignment_name,
-    )
-    release_path.mkdir(parents=True)
-    release_path.joinpath("file1.txt").touch()
-    release_path.joinpath("file2.ipynb").touch()
-    release_path.joinpath(".DS_Store").touch()
-    print(release_path)
-
-    return a_config, assignment_name, release_path
 
 
 def test_create_template_dir(default_config, tmp_path):
