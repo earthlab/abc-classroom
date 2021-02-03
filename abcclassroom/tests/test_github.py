643539e6fa35c4cbbd6eaf7b6dd8a4c3ca8e4644
Karen Cranston <karen.cranston@gmail.com>
2021-02-03 15:46:01 -0500

    temp comment out to make flake8 pass so I can merge

---
+++ abcclassroom/tests/test_github.py
@@ -0,0 +1,20 @@
+# Tests for git and github methods
+# from pathlib import Path
+#
+# import pytest
+#
+# import abcclassroom.github as abcgit
+
+# Methods
+# _call_git
+# remote_repo_exists
+# clone_repo
+# add_remote
+# repo_changed
+# new_branch
+# get_commit_message
+# commit_all_changes
+# init_and_commit
+# push_to_github
+# pull_from_github
+# git_init
