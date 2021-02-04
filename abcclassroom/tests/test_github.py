32986361d269d7c6e088c43a6418a47490ea37ef
Karen Cranston <karen.cranston@gmail.com>
2021-02-04 15:06:24 -0500

    adding tests for master-to-main

--- abcclassroom/tests/test_github.py
+++ abcclassroom/tests/test_github.py
@@ -1,20 +1,50 @@
 # Tests for git and github methods
-# from pathlib import Path
-#
-# import pytest
-#
-# import abcclassroom.github as abcgit
+from pathlib import Path
 
-# Methods
-# _call_git
-# remote_repo_exists
-# clone_repo
-# add_remote
-# repo_changed
-# new_branch
-# get_commit_message
-# commit_all_changes
-# init_and_commit
-# push_to_github
-# pull_from_github
-# git_init
+import abcclassroom.github as abcgit
+
+
+def test_init_and_commit(default_config, tmp_path):
+    """
+    Tests that we can create a directory, initialize it as a git repo,
+    and commit some changes.
+    """
+    repo_dir = Path(tmp_path, "init-and-commit")
+    repo_dir.mkdir()
+    a_file = Path(repo_dir, "testfile.txt")
+    a_file.write_text("Some text")
+    abcgit.init_and_commit(repo_dir)
+    assert Path(repo_dir, ".git").exists()
+    git_return = abcgit._call_git("log", directory=repo_dir)
+    assert git_return.stdout.startswith("commit")
+
+
+def test_master_branch_to_main_repeatedly(tmp_path):
+    """
+    Tests that we can sucessfully change the default master branch to
+    main, and nothing bad happends if we try and do it again
+    """
+    repo_dir = Path(tmp_path, "change-master")
+    repo_dir.mkdir()
+    # we need to make some files and commit in order to rename the branch
+    a_file = Path(repo_dir, "testfile.txt")
+    a_file.write_text("Some text")
+    abcgit.init_and_commit(repo_dir)
+
+    # change the default branch name
+    abcgit._master_branch_to_main(repo_dir)
+    abcgit._call_git("show-ref", "--quiet", "--verify", "refs/heads/main")
+
+    # trying again should also work without error
+    abcgit._master_branch_to_main(repo_dir)
+
+
+def test_master_branch_to_main_no_commits(tmp_path):
+    """
+    Tests that changing the name of the master branch in a initialized
+    repo without commits works.
+    """
+    repo_dir = Path(tmp_path, "change-master-no-commits")
+    repo_dir.mkdir()
+    abcgit.git_init(repo_dir)
+    abcgit._master_branch_to_main(repo_dir)
