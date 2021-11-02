c40291b2a6ab042cf398f32baafc2204c733661c
Leah Wasser <leah.wasser@colorado.edu>
2021-11-01 18:57:56 -0600

    flake8

--- abcclassroom/tests/test_github.py
+++ abcclassroom/tests/test_github.py
@@ -1,16 +1,17 @@
 # Tests for github script
 import sys
 import os
+from pathlib import Path
 from unittest import mock
 
 import pytest
 import github3 as gh3
 
-# TODO:  if we import tests this way we aren't necessarily testing the  dev
-# version
+# TODO:  if we import tests this way we aren't necessarily testing the dev
+# version depending upon how abc is installed
 # of abc classroom
-import abcclassroom.github as github
 import abcclassroom.config as config
+import abcclassroom.github as github
 
 
 # Creating an OrganizationObject that will run instead of
@@ -384,68 +385,70 @@ def test_clone_repo_bad_repo_unittest(
 # CLONE: test what happens when you clone and the remote doesn't exist
 
 
-# This is the old tests
-# from pathlib import Path
-#
-# import abcclassroom.github as abcgit
-#
-#
-# def test_init_and_commit(default_config, tmp_path):
-#     """
-#     Tests that we can create a directory, initialize it as a git repo,
-#     and commit some changes.
-#     """
-#     repo_dir = Path(tmp_path, "init-and-commit")
-#     repo_dir.mkdir()
-#     a_file = Path(repo_dir, "testfile.txt")
-#     a_file.write_text("Some text")
-#     abcgit.init_and_commit(repo_dir)
-#     assert Path(repo_dir, ".git").exists()
-#     git_return = abcgit._call_git("log", directory=repo_dir)
-#     assert git_return.stdout.startswith("commit")
-#
-#
-# def test_master_branch_to_main_repeatedly(tmp_path):
-#     """
-#     Tests that we can sucessfully change the default master branch to
-#     main, and nothing bad happends if we try and do it again
-#     """
-#     repo_dir = Path(tmp_path, "change-master")
-#     repo_dir.mkdir()
-#     abcgit.git_init(repo_dir)
-#
-#     # change the default branch name
-#     abcgit._master_branch_to_main(repo_dir)
-#     # in order to test that main exists, we need to add some commits
-#     a_file = Path(repo_dir, "testfile.txt")
-#     a_file.write_text("Some text")
-#     commit_msg = "commit some things"
-#     abcgit.commit_all_changes(repo_dir, msg=commit_msg)
-#
-#     # now test the existance of main
-#     abcgit._call_git(
-#         "show-ref",
-#         "--quiet",
-#         "--verify",
-#         "refs/heads/main",
-#         directory=repo_dir,
-#     )
-#
-#     # trying again should also work without error
-#     abcgit._master_branch_to_main(repo_dir)
-#
-#
-# def test_master_branch_to_main_no_commits(tmp_path):
-#     """
-#     Tests that changing the name of the master branch in a initialized
-#     repo without commits works.
-#     """
-#     repo_dir = Path(tmp_path, "change-master-no-commits")
-#     repo_dir.mkdir()
-#     abcgit.git_init(repo_dir)
-#     abcgit._master_branch_to_main(repo_dir)
+# These are tests in the main branch now
+# i think i mistakenly thought these were from the old abc classroom they
+# are more likely from karen!
 
 
+def test_init_and_commit(default_config, tmp_path):
+    """
+    Tests that we can create a directory, initialize it as a git repo,
+    and commit some changes.
+    """
+    repo_dir = Path(tmp_path, "init-and-commit")
+    repo_dir.mkdir()
+    a_file = Path(repo_dir, "testfile.txt")
+    a_file.write_text("Some text")
+    github.init_and_commit(repo_dir)
+    assert Path(repo_dir, ".git").exists()
+    git_return = github._call_git("log", directory=repo_dir)
+    assert git_return.stdout.startswith("commit")
+
+
+def test_master_branch_to_main_repeatedly(tmp_path):
+    """
+    Tests that we can sucessfully change the default master branch to
+    main, and nothing bad happends if we try and do it again
+    """
+    repo_dir = Path(tmp_path, "change-master")
+    repo_dir.mkdir()
+    github.git_init(repo_dir)
+
+    # change the default branch name
+    github._master_branch_to_main(repo_dir)
+    # in order to test that main exists, we need to add some commits
+    a_file = Path(repo_dir, "testfile.txt")
+    a_file.write_text("Some text")
+    commit_msg = "commit some things"
+    github.commit_all_changes(repo_dir, msg=commit_msg)
+
+    # now test the existance of main
+    github._call_git(
+        "show-ref",
+        "--quiet",
+        "--verify",
+        "refs/heads/main",
+        directory=repo_dir,
+    )
+
+    # trying again should also work without error
+    github._master_branch_to_main(repo_dir)
+
+
+def test_master_branch_to_main_no_commits(tmp_path):
+    """
+    Tests that changing the name of the master branch in a initialized
+    repo without commits works.
+    """
+    repo_dir = Path(tmp_path, "change-master-no-commits")
+    repo_dir.mkdir()
+    github.git_init(repo_dir)
+    github._master_branch_to_main(repo_dir)
+
+
+"""
+>>>>>> TESTS BELOW ARE ON FUNCTIONS THAT I THINK ARE DEPRECATED <<<<<
+"""
 # TODO: https://github.com/earthlab/abc-classroom/issues/361 this function
 # isnt' in karen's list of functions to test
 # def test_check_student_repo_exists_pass(monkeypatch):
